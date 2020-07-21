50af70bc99c19f314d1d104d3b55ba34387919b9
Shekhar Jain <shekhar@jain.la>
2020-07-21 15:40:48 -0700

    Final Production

--- src/main/java/la/jain/configclient/ConfigClientApplication.java
+++ src/main/java/la/jain/configclient/ConfigClientApplication.java
@@ -1,8 +1,16 @@
 package la.jain.configclient;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@EnableAutoConfiguration(exclude = {
+		org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class
+})
 @SpringBootApplication
 public class ConfigClientApplication {
 
@@ -10,4 +18,16 @@ public class ConfigClientApplication {
 		SpringApplication.run(ConfigClientApplication.class, args);
 	}
 
+	@RefreshScope
+	@RestController
+	class MessageRestController {
+
+		@Value("${message:Hello default}")
+		private String message;
+
+		@RequestMapping("/message")
+		String getMessage() {
+			return this.message;
+		}
+	}
 }

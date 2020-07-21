5aeb502e82ef040352888c632ed9de8f1fe88d6f
Shekhar Jain <shekhar@jain.la>
2020-07-21 13:17:30 -0700

    First Commit

---
+++ src/test/java/la/jain/configclient/ConfigClientApplicationTests.java
@@ -0,0 +1,13 @@
+package la.jain.configclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
